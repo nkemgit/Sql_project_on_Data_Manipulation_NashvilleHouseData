9293f65321b02d56527db938ffba0f23fc9f1753
Nkemjika Nwanebu <119932254+nkemdata@users.noreply.github.com>
2024-11-07 05:27:19 +0100

    commit change

--- Sql_project/1_creating_of_stagging_data.sql
+++ Sql_project/1_creating_of_stagging_data.sql
@@ -2,6 +2,15 @@
 --we create  stagging dataset, it literlly mean dubplicating the raw data for purpose of backup
 DROP TABLE IF EXISTS stagging_nsh;
 
+/*
+-- CLEANING EXPLORATION 
+# create staging table 
+# Remve duplicate
+# standardise the dataset
+# Null values or Bank row
+# Remove unwanted column and rows
+*/
+
 SELECT *
 INTO apple.dbo.stagging_nsh
 FROM apple.dbo.nashville_housing_data;
