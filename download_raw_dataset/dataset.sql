5f59cc47af61642737ff290e30c386e0f9c0ddf5
Nkemjika Nwanebu <119932254+nkemdata@users.noreply.github.com>
2024-11-07 05:46:52 +0100

    commit

--- download_raw_dataset/dataset.sql
+++ download_raw_dataset/dataset.sql
@@ -2,10 +2,15 @@
 --DATA CLEANING OF HOUSING DATA, Download the dateset [https://tinyurl.com/ep22zk7e]
 
 /*
-Housing Data Cleaning Project
+-- CLEANING EXPLORATION INVOLVED
+# create staging table 
+# Remve duplicate
+# standardise the dataset
+# Null values or Bank row
+# Remove unwanted column and rows
+
 Skills used: Joins, CTE's, Windows Functions,  Converting Data Types, Date Formating
 */
-
 
 SELECT *
 FROM apple.dbo.nashville_housing_data;
